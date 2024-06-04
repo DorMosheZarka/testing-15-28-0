e6acd5a6170d6253b74960c9ece2fea864781867
dorza <dorza@wix.com>
2024-06-04 18:24:42 +0300

    chore(): add feature to app

--- src/App.tsx
+++ src/App.tsx
@@ -2,10 +2,12 @@ import styles from './App.module.scss';
 import ViteSvg from './assets/vite.svg';
 import TypescriptSvg from './assets/typescript.svg';
 import { Cards } from './components/cards/cards';
+import { Header } from './components/header/header';
 
 function App() {
     return (
         <div className={styles.root}>
+            <Header />
             <Cards />
         </div>
     );
