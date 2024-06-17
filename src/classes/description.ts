4ccb51a541d427c05eb4f2399aef1cf36415c3b0
dorza <dorza@wix.com>
2024-06-17 20:45:42 +0300

    checkpoint

--- src/classes/description.ts
+++ src/classes/description.ts
@@ -2,18 +2,27 @@ export class Description {
     name;
     companyName;
     since;
-    subDescription;
+    param1;
+    param2;
+    param3;
+    param4;
 
     constructor(
         name: string,
         companyName: string,
         since: number,
-        subDescription?: Description,
+        param1: any,
+        param2: any,
+        param3: any,
+        param4: any,
     ) {
         this.name = name;
         this.companyName = companyName;
         this.since = since;
-        this.subDescription = subDescription;
+        this.param1 = param1;
+        this.param2 = param2;
+        this.param3 = param3;
+        this.param4 = param4;
     }
 
     getPerson() {
