1f355ff8159b42468efae7e83b0d5baa960d2629
dorza <dorza@wix.com>
2024-06-17 20:07:10 +0300

    checkpoint

--- src/classes/description.ts
+++ src/classes/description.ts
@@ -1,17 +1,21 @@
-type Since =  number | string;
-
 export class Description {
     name;
     companyName;
     since;
+    subDescription;
 
-    constructor(name: string, companyName: string, since?: Since) {
+    constructor(name: string, companyName: string, since?: number, subDescription?: Description) {
         this.name = name;
         this.companyName = companyName;
         this.since = since;
+        this.subDescription = subDescription;
     }
 
     getPerson() {
         return `${this.name} ${this.companyName} ${this.since}`;
+    }
+
+    getSubDescription() {
+        return this.subDescription;
     }
 }
