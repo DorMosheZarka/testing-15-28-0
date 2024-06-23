7f2d79de9e0eee0da8dc726cde5eecc4c6eb978a
dorza <dorza@wix.com>
2024-06-23 16:52:51 +0300

    checkpoint

--- src/App.tsx
+++ src/App.tsx
@@ -4,13 +4,21 @@ import TypescriptSvg from './assets/typescript.svg';
 import { Cards } from './components/cards/cards';
 import { Header } from './components/header/header';
 import { HeroSection } from './components/hero-section/hero-section';
+import { Dialog } from './components/dialog/dialog';
+
+const wah: string = 'wah';
 
 function App() {
     return (
         <div>
-            <HeroSection imgUrl={'https://wixplosives.github.io/codux-assets-storage/add-panel/image-placeholder.jpg'} />
+            <HeroSection
+                imgUrl={
+                    'https://wixplosives.github.io/codux-assets-storage/add-panel/image-placeholder.jpg'
+                }
+            />
             <Header />
             <Cards />
+            <Dialog title="This is a title" text="this is a text" buttonAcceptText={wah} />
         </div>
     );
 }
