04fe30a0779eec1f7a1d115ad1119fbfc666d3b3
dorza <dorza@wix.com>
2024-06-30 16:12:17 +0300

    checkpoint

---
+++ src/_codux/boards/all-react-nodes-list/all-react-nodes-list.board.tsx
@@ -0,0 +1,33 @@
+import { createBoard } from '@wixc3/react-board';
+import { AllReactNodesList } from '../../../components/all-react-nodes-list/all-react-nodes-list';
+
+const stringInstance: string = 'stringInstance';
+const numberInstance: number = 0;
+const booleanInstance: boolean = true;
+const undefinedInstance: undefined = undefined;
+const nullInstance: null = null;
+const reactElementInstance: React.ReactElement = <>this is a react element instance</>;
+const reactElementArrayInstance: React.ReactElement[] = [
+    <li key="1">{stringInstance}</li>,
+    <li key="2">{numberInstance}</li>,
+    <li key="3">{booleanInstance} (not `Error` = works)</li>,
+    <li key="4">{undefinedInstance} (not `Error` = works)</li>,
+    <li key="5">{nullInstance} (not `Error` = works)</li>,
+    <li key="6">{reactElementInstance}</li>,
+];
+
+export default createBoard({
+    name: 'AllReactNodesList',
+    Board: () => (
+        <AllReactNodesList
+            stringInstance={stringInstance}
+            numberInstance={numberInstance}
+            booleanInstance={booleanInstance}
+            undefinedInstance={undefinedInstance}
+            nullInstance={nullInstance}
+            reactElementInstance={reactElementInstance}
+            reactElementArrayInstance={reactElementArrayInstance}
+        />
+    ),
+    isSnippet: true,
+});
---
+++ src/components/all-react-nodes-list/all-react-nodes-list.tsx
@@ -0,0 +1,43 @@
+import classNames from 'classnames';
+import styles from './all-react-nodes-list.module.scss';
+import React from 'react';
+
+export interface AllReactNodesListProps {
+    className?: string;
+    stringInstance: string; // should default to "Default Text" ✔️
+    numberInstance: number; // should default to 0 ✔️
+    booleanInstance: boolean; // should default to false ✔️
+    undefinedInstance: undefined; // should default to undefined ✔️
+    nullInstance: null; // should default to undefined ✔️
+    reactElementInstance: React.ReactElement; // should default to undefined ✔️
+    reactElementArrayInstance: React.ReactElement[]; // should default to undefined ✔️
+}
+
+export const AllReactNodesList = ({
+    className,
+    stringInstance,
+    numberInstance,
+    booleanInstance,
+    undefinedInstance,
+    nullInstance,
+    reactElementInstance,
+    reactElementArrayInstance,
+}: AllReactNodesListProps) => {
+    return (
+        <div className={classNames(styles.root, className)}>
+            <h3>This is a list of all matching ReactNode types</h3>
+            <ul>
+                <li>{stringInstance}</li>
+                <li>{numberInstance}</li>
+                <li>{booleanInstance} (not `Error` = works)</li>
+                <li>{undefinedInstance} (not `Error` = works)</li>
+                <li>{nullInstance} (not `Error` = works)</li>
+                <li>{reactElementInstance}</li>
+                <li>
+                    Array of React Elements:
+                    <ul>{reactElementArrayInstance}</ul>
+                </li>
+            </ul>
+        </div>
+    );
+};
